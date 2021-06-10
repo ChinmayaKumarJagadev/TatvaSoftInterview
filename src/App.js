443bd740ec179deb0c424288bf8344663dd740e7
Chinmaya Jagadev <chinmaya.jagadev@indusnet.co.in>
2021-06-11 00:45:26 +0530

    add pagination and searchFilter

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import {useSelector,useDispatch} from 'react-redux'
 import {userDataRecords} from './actions/index'
 import axios from 'axios';
 // import ReactTable from 'react-table'
-
+import Pagination from './Pagination/Pagination'
 import './App.css';
 
 const App = () =>{
   const [searchItem, setSearchItem] =useState('')
-
+  const [pageCount, setPageCount] = useState(0)
+  const userPerPage = 10;
+  const pageVisit = pageCount * userPerPage
+    
   const userData = useSelector(state => state.allUserData.userInfo)
   console.log(userData)
   const dispatch= useDispatch()
@@ -26,43 +29,68 @@ const App = () =>{
     })
   }
 
-
   useEffect (() =>{
     fetchUserdata()
   },[])
 
-  
-const header= ["Gender","Email","City","Name"]
+
+
+//setting up the Headers
+const allheader = () =>{
+  const header = ['Gender', 'Email', 'Name', 'Picture']
+  return header.map((headers,index) =>{
+    return (
+      <th key={index}>{headers.toUpperCase()}</th>
+    )
+  })
+}
+
+
+
+ // For rendering Pagenumber for all Pages
+ const totalPageCount = Math.ceil(userData.length / userPerPage) 
+ const handlePagination = ({selected}) =>{
+    setPageCount(selected)
+  }
+
   return (
     <div className="App">
       <p>React</p>
      
-      <input type="text" name="search"  onChange={event =>{setSearchItem(event.target.value)}}/>
-      {userData.filter((data) =>{
-          if(searchItem == "")
-          return data
-          else if(data.email.toLowerCase().includes(searchItem.toLocaleLowerCase())){
-            return data
-          }
-      }).map((data, id) =>{
-
-        const {first, last} = data.name
-        const {gender,email,city,name} = data;
-        return(
+       <input type="text" name="search" onChange={(e) =>setSearchItem(e.target.value)}/>
           <div>
           
             <table className="table">
-              
-              <td>{gender}</td>
-              <td>{email}</td>
-              <td>{city}</td>
-              <td>{name.first}{name.last}</td>
+              <thead>
+                <tr>{allheader()}</tr>
+              </thead>
+
+              <tbody>              
+                 {userData.slice(pageVisit, pageVisit + userPerPage).filter((data) =>{
+                    //console.log(data)
+                     if(searchItem == "")
+                        return data
+                     else if(data.email.toLowerCase().includes(searchItem.toLocaleLowerCase())){
+                        return data
+                      }
+                 }).map(({gender,email,name,picture}) =>{
+   
+                    return(
+                         <tr>
+                         <td>{gender}</td>
+                         <td>{email}</td>
+                         <td>{name.first}{name.last}</td>
+                         <td><img src={picture.medium} /></td>
+                         </tr>
+                        )
+                    })
+                 }
+            </tbody>             
             </table>
            
+           <Pagination pageCount={totalPageCount} onClick={handlePagination}/>
           </div>
-        )
-      })}
-    </div>
+        </div>
   );
 }
 
